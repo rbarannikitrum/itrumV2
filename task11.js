38b92868b59192df1707125d254f4e33f6b6fb92
rbarannikirum <rbarannikitrum@gmail.com>
2022-09-28 16:51:01 +0300

    third module

---
+++ task11.js
@@ -0,0 +1,14 @@
+// Дана строка. Написать функцию, которая определит, содержит ли строка только символы 'a', 'b', 'c' или нет.
+function checkSymbols(str) {
+    let result = true
+    for (let i = 0; i < str.length; i++) {
+        if (!(str[i] === 'a' || str[i] === 'b' || str[i] === 'c')) {
+            result = false
+            break
+        }
+    }
+
+    return result
+}
+
+console.log(checkSymbols("abcbacabcqbcabcbnaba"))
