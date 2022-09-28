2e0fe52e961aa9f6fb9f8f2809f15741556a47c7
rbarannikirum <rbarannikitrum@gmail.com>
2022-09-28 17:04:18 +0300

    remade task5

--- task5.js
+++ task5.js
@@ -3,26 +3,26 @@
 
 function stringByThree(str) {
     const arr = str.split('')
-    const join_arr = []
+    const joinArr = []
     const result = []
     for (let i = 1; i < arr.length; i = i + 3) {
         arr[i] = '_'
     }
     for (let i = 0; i < arr.length; i = i + 3) {
-        join_arr.push([arr[i], arr[i + 1], arr[i + 2]])
+        joinArr.push([arr[i], arr[i + 1], arr[i + 2]])
 
     }
-    for (let j = join_arr.length - 1; j > 0; j--) {
+    for (let j = joinArr.length - 1; j > 0; j--) {
         for (let i = 0; i < j; i++) {
-            if (join_arr[i] > join_arr[i + 1]) {
-                let temp = join_arr[i];
-                join_arr[i] = join_arr[i + 1];
-                join_arr[i + 1] = temp;
+            if (joinArr[i] > joinArr[i + 1]) {
+                let temp = joinArr[i];
+                joinArr[i] = joinArr[i + 1];
+                joinArr[i + 1] = temp;
             }
         }
     }
-    for (let i = 0; i < join_arr.length; i++) {
-        result.push(join_arr[i].join(''))
+    for (let i = 0; i < joinArr.length; i++) {
+        result.push(joinArr[i].join(''))
     }
 
     return result
