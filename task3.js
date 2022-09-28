59c6c565e093ceb66db18b2bf2ea1537ae589a27
rbarannikirum <rbarannikitrum@gmail.com>
2022-09-28 16:59:51 +0300

    remade task3

--- task3.js
+++ task3.js
@@ -1,33 +1,19 @@
 // Напишите функцию removeDuplicates(arr), которая возвращает массив,
 // в котором удалены повторяющиеся элементы из массива arr (игнорируйте чувствительность к регистру).
-
 function removeDuplicates(arr) {
     for (let i = 0; i < arr.length; i++) {
-        if (typeof (arr[i]) === 'string') {
-            arr[i] = arr[i].toLowerCase()
-        }
-    }
-    for (let j = arr.length - 1; j > 0; j--) {
-        for (let i = 0; i < j; i++) {
-            if (arr[i] > arr[i + 1]) {
-                let temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
+        let counter = 0
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                counter++
+                if (counter > 1) {
+                    arr.splice(j, 1)
+                }
             }
         }
     }
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === arr[i + 1]) {
-            arr.splice(i + 1, 1)
-        }
-    }
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === arr[i - 1]) {
-            arr.splice(i - 1, 1)
-        }
-    }
-
     return arr
 }
+
 
 console.log(removeDuplicates([4, 7, 1, 9, 6, 8, 4, 6, 3, 6]))
