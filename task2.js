b31574bcdae0e814fadab76dc0acaa0539b9a23f
rbarannikirum <rbarannikitrum@gmail.com>
2022-09-30 11:10:59 +0300

    remade task2

--- task2.js
+++ task2.js
@@ -1,14 +1,13 @@
 // Дано число, например 31. Проверьте, что это число не делится ни на одно другое число кроме себя самого и единицы.
 // То есть в нашем случае нужно проверить, что число 31 не делится на все числа от 2 до 30. Если число не делится - выведите 'false', а если делится - выведите 'true'.
 function simpleNumber(num) {
-    let isDivided = false
     for (let i = 2; i < num; i++) {
         if (num % i === 0) {
-            isDivided = true
+            return true
         }
     }
 
-    return isDivided
+    return false
 }
 
-console.log(simpleNumber(4))
+console.log(simpleNumber(31))
