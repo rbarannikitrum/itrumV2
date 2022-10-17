4b7fab757670d9e04f9cde71a250465d2b014c71
rbarannikirum <rbarannikitrum@gmail.com>
2022-10-17 19:01:39 +0300

    some fixes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let taskArr = []
 let text
 
 
+
 function getTask() {
 
   taskInput = document.querySelector("input").value
@@ -13,22 +14,29 @@ function getEdit() {
 }
 
 function setEditedTask(i) {
+  if (!text) {
+    text = taskArr[i].text
+  }
   taskArr[i].text = text
   taskArr[i].edit = !taskArr[i].edit
   render()
 }
 
 function addTask() {
-  if (taskInput.length === 0) {
-    return 0
+  if (!taskInput.length) {
+    return
+  }
+  if (!taskInput.trim()) {
+    return
   }
   taskInput = taskInput.trim()
-  taskArr.push({text: taskInput, completed: false, edit : false})
+  taskArr.push({text: taskInput, completed: false})
   render()
+  document.querySelector('input').focus()
+  document.querySelector('input').value = ''
 }
 
 function deleteElem(i) {
-
   taskArr.splice(i, 1)
   render()
 }
@@ -38,24 +46,25 @@ function setActive(i) {
   render()
 }
 function setEdit (i) {
-  taskArr[i].edit = !taskArr[i].edit
   render()
+  document.getElementById(`task-${i}`).classList.toggle('hidden')
+  document.getElementById(`edit-${i}`).classList.toggle('active')
 }
+
 
 function render() {
   let elem = ''
   taskArr = taskArr.sort((a, b) => a.completed - b.completed)
   taskArr.forEach((el, i) => {
-    elem += el.edit ?
-`<div class="edit">
+    elem += `<div class="edit hidden" id = 'edit-${i}' >
 <input id = 'change' type="text" value="${taskArr[i].text}" onchange="getEdit()" class="edit_text"/>
 <div class="operators">
 <button onclick="setEditedTask(${i})">Save</button>
-<button onclick="setEdit(${i})">Cancel</button>
+<button onclick="render()">Cancel</button>
 </div>
-</div>`
-:
-`<div id = '${i}' class="task">
+
+</div>
+<div id = 'task-${i}' class="task">
 <div class="task_text">
 <p style="${el.completed ? 'text-decoration : line-through' : ''}">${i+1}.${el.text}</p>
 </div>
@@ -66,8 +75,10 @@ function render() {
 
 <button style=" ${el.completed ? 'display : none' : ''}" onclick="setEdit(${i})">Edit</button>
 <button onclick="deleteElem(${i})">Delete</button></div>
-</div>`
+</div>
+
+`
 
   })
-  document.getElementById('lol').innerHTML = elem
+  document.querySelector('#tasks').innerHTML = elem
 }
