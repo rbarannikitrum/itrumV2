54c58517e96ca08af6ff956b190f108f3ee36da7
rbarannikirum <rbarannikitrum@gmail.com>
2022-10-19 16:50:52 +0300

    localstorage

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 let taskInput
-let taskArr = []
+let taskArr = JSON.parse(localStorage.getItem('tasks')) || []
 let text
 
 
-
 function getTask() {
-
   taskInput = document.querySelector("input").value
 }
 
@@ -14,15 +12,18 @@ function getEdit() {
 }
 
 function setEditedTask(i) {
+  getEdit()
   if (!text) {
     text = taskArr[i].text
   }
   taskArr[i].text = text
   taskArr[i].edit = !taskArr[i].edit
+  localStorage.setItem('tasks', JSON.stringify(taskArr))
   render()
 }
 
 function addTask() {
+  getTask()
   if (!taskInput.length) {
     return
   }
@@ -34,22 +35,29 @@ function addTask() {
   render()
   document.querySelector('input').focus()
   document.querySelector('input').value = ''
+  localStorage.setItem('tasks', JSON.stringify(taskArr))
+
 }
 
 function deleteElem(i) {
   taskArr.splice(i, 1)
   render()
+  localStorage.setItem('tasks', JSON.stringify(taskArr))
+
 }
 
 function setActive(i) {
   taskArr[i].completed = !taskArr[i].completed
   render()
+  localStorage.setItem('tasks', JSON.stringify(taskArr))
+
 }
 function setEdit (i) {
   render()
   document.getElementById(`task-${i}`).classList.toggle('hidden')
   document.getElementById(`edit-${i}`).classList.toggle('active')
 }
+
 
 
 function render() {
@@ -59,6 +67,8 @@ function render() {
     elem += `<div class="edit hidden" id = 'edit-${i}' >
 <input id = 'change' type="text" value="${taskArr[i].text}" onchange="getEdit()" class="edit_text"/>
 <div class="operators">
+<input type="checkbox" onclick="setActive(${i})" ${el.completed ? 'checked' : ''} style="width: 20px; height: 20px;">
+
 <button onclick="setEditedTask(${i})">Save</button>
 <button onclick="render()">Cancel</button>
 </div>
@@ -66,7 +76,7 @@ function render() {
 </div>
 <div id = 'task-${i}' class="task">
 <div class="task_text">
-<p style="${el.completed ? 'text-decoration : line-through' : ''}">${i+1}.${el.text}</p>
+<p style="${el.completed ? 'text-decoration : line-through; color : #5C6672' : ''}">${i+1}.${el.text}</p>
 </div>
 <div class="operators">
 
