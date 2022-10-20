99c370c746d7bba4a162f4bcabd28a92d1a82a2c
rbarannikirum <rbarannikitrum@gmail.com>
2022-10-20 14:25:51 +0300

    todo update fetch

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let taskInput
-let taskArr = JSON.parse(localStorage.getItem('tasks')) || []
+let taskArr = []
 let text
 
+async function fetchData () {
+  taskArr = await fetch('http://localhost:8000/allTasks', {method : 'GET'}).then(res => res.json()).then(res => res.data)
+  render()
+}
 
 function getTask() {
   taskInput = document.querySelector("input").value
@@ -11,18 +15,18 @@ function getEdit() {
   text = document.getElementById('change').value
 }
 
-function setEditedTask(i) {
+async function setEditedTask(i) {
   getEdit()
   if (!text) {
     text = taskArr[i].text
   }
   taskArr[i].text = text
-  taskArr[i].edit = !taskArr[i].edit
-  localStorage.setItem('tasks', JSON.stringify(taskArr))
+  taskArr = await fetch('http://localhost:8000/updateTask', {method : 'PATCH', body : JSON.stringify({id: taskArr[i].id, text: text, isCheck: false}), headers : {"Content-Type" : "application/json;charset=utf-8", "Access-Control-Allow-Origin" : "*"}}).then(res => res.json()).then(res => res.data)
   render()
+  console.log(taskArr)
 }
 
-function addTask() {
+async function addTask() {
   getTask()
   if (!taskInput.length) {
     return
@@ -31,26 +35,23 @@ function addTask() {
     return
   }
   taskInput = taskInput.trim()
-  taskArr.push({text: taskInput, completed: false})
-  render()
   document.querySelector('input').focus()
   document.querySelector('input').value = ''
-  localStorage.setItem('tasks', JSON.stringify(taskArr))
-
+  taskArr = await fetch('http://localhost:8000/createTask', {method : 'POST', body : JSON.stringify({text: taskInput, isCheck: false}), headers : {"Content-Type" : "application/json;charset=utf-8", "Access-Control-Allow-Origin" : "*"}}).then(res => res.json()).then(res => res.data)
+  render()
+  console.log(taskArr)
 }
 
-function deleteElem(i) {
-  taskArr.splice(i, 1)
+async function deleteElem(i) {
+  taskArr = await fetch(`http://localhost:8000/deleteTask?id=${taskArr[i].id}`, {method : 'DELETE'}).then(res => res.json()).then(res => res.data)
+  console.log(taskArr)
   render()
-  localStorage.setItem('tasks', JSON.stringify(taskArr))
-
 }
 
-function setActive(i) {
-  taskArr[i].completed = !taskArr[i].completed
+async function setActive(i) {
+  taskArr = await fetch('http://localhost:8000/updateTask', {method : 'PATCH', body : JSON.stringify({id: taskArr[i].id, text: taskArr[i].text, isCheck: !taskArr[i].isCheck}), headers : {"Content-Type" : "application/json;charset=utf-8", "Access-Control-Allow-Origin" : "*"}}).then(res => res.json()).then(res => res.data)
+  console.log(taskArr)
   render()
-  localStorage.setItem('tasks', JSON.stringify(taskArr))
-
 }
 function setEdit (i) {
   render()
@@ -62,12 +63,12 @@ function setEdit (i) {
 
 function render() {
   let elem = ''
-  taskArr = taskArr.sort((a, b) => a.completed - b.completed)
+  taskArr = taskArr.sort((a, b) => a.isCheck - b.isCheck)
   taskArr.forEach((el, i) => {
     elem += `<div class="edit hidden" id = 'edit-${i}' >
 <input id = 'change' type="text" value="${taskArr[i].text}" onchange="getEdit()" class="edit_text"/>
 <div class="operators">
-<input type="checkbox" onclick="setActive(${i})" ${el.completed ? 'checked' : ''} style="width: 20px; height: 20px;">
+<input type="checkbox" onclick="setActive(${i})" ${el.isCheck ? 'checked' : ''} style="width: 20px; height: 20px;">
 
 <button onclick="setEditedTask(${i})">Save</button>
 <button onclick="render()">Cancel</button>
@@ -76,14 +77,14 @@ function render() {
 </div>
 <div id = 'task-${i}' class="task">
 <div class="task_text">
-<p style="${el.completed ? 'text-decoration : line-through; color : #5C6672' : ''}">${i+1}.${el.text}</p>
+<p style="${el.isCheck ? 'text-decoration : line-through; color : #5C6672' : ''}">${i+1}.${el.text}</p>
 </div>
 <div class="operators">
 
-<input type="checkbox" onclick="setActive(${i})" ${el.completed ? 'checked' : ''} style="width: 20px; height: 20px;">
+<input type="checkbox" onclick="setActive(${i})" ${el.isCheck ? 'checked' : ''} style="width: 20px; height: 20px;">
 
 
-<button style=" ${el.completed ? 'display : none' : ''}" onclick="setEdit(${i})">Edit</button>
+<button style=" ${el.isCheck ? 'display : none' : ''}" onclick="setEdit(${i})">Edit</button>
 <button onclick="deleteElem(${i})">Delete</button></div>
 </div>
 
