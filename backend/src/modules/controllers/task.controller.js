62f8f9a49a8b2a2156719fa488fd7d4edd74b0a6
rbarannikirum <rbarannikitrum@gmail.com>
2022-10-27 13:01:56 +0300

    final commit

--- backend/src/modules/controllers/task.controller.js
+++ backend/src/modules/controllers/task.controller.js
@@ -1,36 +1,57 @@
 const Task = require('../../db/models/task/index')
 
-module.exports.getAllTasks = (req, res, next) => {
-  Task.find().then(result => {
-    res.send(result)
-  })
-      .catch(error => res.status(500).send('server error'))
+module.exports.getAllTasks = (req, res) => {
+  try {
+    Task.find().then(result => {
+      res.send(result)
+    })
+  }
+ catch (error) {
+   res.status(500).send(`server error : ${error}`)
+ }
 }
 
-module.exports.createNewTask = (req, res, next) => {
-  if (typeof req.body.isCheck === 'boolean' &&
-      req.body.text &&
-      typeof req.body.text === 'string') {
-    const task = new Task(req.body)
-    task.save()
-        .then(result => res.send(result))
-        .catch(error => next(error))
-  } else res.status(400).send('error')
+module.exports.createNewTask = (req, res) => {
+  try {
+    if (typeof req.body.isCheck === 'boolean' &&
+        req.body.text &&
+        typeof req.body.text === 'string') {
+      const task = new Task(req.body)
+      task.save()
+          .then(result => res.send(result))
+          .catch(error => res.send(error))
+    } else res.status(400).send('error')
+  }
+  catch (error) {
+    res.status(500).send(`server error : ${error}`)
+  }
+
 }
 
 module.exports.changeTaskInfo = (req, res, next) => {
-  if (typeof req.body.isCheck === 'boolean' &&
-      req.body.text &&
-      typeof req.body.text === 'string') {
-    Task.findByIdAndUpdate(req.body.id, {text: req.body.text, isCheck: req.body.isCheck})
-        .then(result => res.send(result))
-        .catch(error => next(error))
-  } else res.status(400).send('error')
+  try {
+    if (typeof req.body.isCheck === 'boolean' &&
+        req.body.text &&
+        typeof req.body.text === 'string') {
+      Task.findByIdAndUpdate(req.body.id, {text: req.body.text, isCheck: req.body.isCheck})
+          .then(result => res.send(result))
+          .catch(error => next(error))
+    } else res.status(400).send('error')
+  }
+  catch (error) {
+    res.status(500).send(`error : ${error}`)
+  }
+
 
 }
 
-module.exports.deleteTask = (req, res, next) => {
-  Task.findByIdAndDelete({_id: req.query.id})
-      .then(result => res.send(result))
-      .catch(res.status(500).send('task not found'))
+module.exports.deleteTask = (req, res) => {
+  try {
+    Task.findByIdAndDelete({_id: req.query.id})
+        .then(result => res.send(result))
+  }
+  catch (error) {
+    res.status(500).send(`error : ${error}`)
+  }
+
 }
