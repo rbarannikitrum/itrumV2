2240e039f1ec3b1ee3575cdc35c21989f4179b2d
rbarannikirum <rbarannikitrum@gmail.com>
2022-10-28 11:22:48 +0300

    final commit

--- backend/src/modules/controllers/spend.controller.js
+++ backend/src/modules/controllers/spend.controller.js
@@ -1,21 +1,62 @@
 const Spend = require('../../db/models/spend/index')
 
 module.exports.createSpend = (req, res) => {
-  req.body.time = new Date()
-  const spend = new Spend(req.body)
-  spend.save().then(result => res.send(result))
+  try {
+    if (typeof req.body.place === 'string' &&
+        req.body.place &&
+        req.body.price &&
+        req.body.price > 0
+    ) {
+      req.body.time = new Date()
+      const spend = new Spend(req.body)
+      spend.save().then(result => res.send(result))
+    } else {
+      res.status(400).send ('uncorrected data')
+    }
+  }
+  catch (error) {
+    res.status(500).send(`sercer error : ${error}`)
+  }
+
+
 }
 
 module.exports.getAllSpends = (req, res) => {
-  Spend.find().then(result => {
-    res.send(result)
-  })
+  try {
+    Spend.find().then(result => {
+      res.send(result)
+    })
+  }
+  catch (error) {
+    res.status(500).send(`server error : ${error}`)
+  }
+
 }
 
 module.exports.updateSpend = (req, res) => {
-  Spend.findByIdAndUpdate(req.body._id, req.body).then(result => res.send(result))
+  try {
+    const date = new Date(req.body.time)
+    if (typeof req.body.place === 'string' &&
+        req.body.place &&
+        req.body.price &&
+        req.body.price > 0 &&
+        date != 'Invalid Date'
+    ) {
+      console.log(req.body._id, req.body)
+      Spend.findByIdAndUpdate(req.body._id, req.body).then(result => res.send(result))
+    } else res.status(400).send('uncorrected data')
+  }
+  catch (error) {
+    res.status(500).send(`server error : ${error}`)
+  }
+
 }
 
 module.exports.deleteSpend = (req, res) => {
-  Spend.findByIdAndDelete({_id: req.query._id}).then(result => res.send(result))
+  try {
+    Spend.findByIdAndDelete({_id: req.query._id}).then(result => res.send(result))
+  }
+  catch (error) {
+    res.status(500).send(`server error : ${error}`)
+  }
 }
