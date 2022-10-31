68a3a74a2c8abc6c1e4c5faf3a80cd8f9c16e612
rbarannikirum <rbarannikitrum@gmail.com>
2022-10-31 10:44:39 +0300

    refactor code backend

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const  apiRoutes = require('./modules/routes/routes')
+const  apiRoutes = require('./src/modules/routes/routes')
 app.use(cors())
 app.use(express.json())
 app.use('/', apiRoutes)
 
+
+console.log(process.env.URI_SPEND)
 mongoose.connect (process.env.URI_SPEND, {useNewURLParser: true, useUnifiedTopology: true})
 
 
