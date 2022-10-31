c1c61b6c90c2927809897935cb7fd9da48881221
rbarannikirum <rbarannikitrum@gmail.com>
2022-10-31 14:17:55 +0300

    refactor code backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 require('dotenv').config({path:'.env'});
 const express = require('express')
 const mongoose = require('mongoose')
-const app = express()
 const cors = require('cors')
 const apiRoutes = require('./src/modules/routes/routes')
+const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/', apiRoutes)
