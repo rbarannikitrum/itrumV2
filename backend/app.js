b70e09b82cdc675d2e1160d3af30f67e69264fc4
rbarannikirum <rbarannikitrum@gmail.com>
2022-10-31 10:37:46 +0300

    refactor code backend

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
 require('dotenv').config({path:'.env'});
-
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
-const apiRoutes = require('./modules/routes/routes')
+const apiRoutes = require('./src/modules/routes/routes')
 app.use(cors())
 app.use(express.json())
 app.use('/', apiRoutes)
+
 
 mongoose.connect(process.env.URI_TODO, {useNewURLParser: true, useUnifiedTopology: true})
 
