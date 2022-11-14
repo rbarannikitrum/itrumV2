387e79faf956294f451f251516bac51c63d9cadc
rbarannikitrum <113897583+rbarannikitrum@users.noreply.github.com>
2022-11-14 10:37:41 +0300

    Delete backend directory

--- backend/src/db/models/spend/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const spendScheme = new mongoose.Schema({
-  place: String,
-  time: Date,
-  price: Number
-})
-
-  module.exports = Spend = mongoose.model('spends', spendScheme)
